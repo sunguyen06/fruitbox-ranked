e3db393ba964d467a8848e752c70ed51d5f7f2a8
Su Nguyen <nguyen.dnt06@gmail.com>
2026-04-24 14:16:13 -0400

    removed port for render service

--- server/config.ts
+++ server/config.ts
@@ -5,7 +5,7 @@ export interface ServerConfig {
 
 export function getServerConfig(): ServerConfig {
   return {
-    port: parsePort(process.env.SOCKET_SERVER_PORT, 3001),
+    port: parsePort(process.env.PORT ?? process.env.SOCKET_SERVER_PORT, 3001),
     corsOrigin: process.env.SOCKET_SERVER_CORS_ORIGIN ?? "http://localhost:3000",
   };
 }
